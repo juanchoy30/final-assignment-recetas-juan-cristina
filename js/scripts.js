1f801cf9f71ed0942ddb1cb2eeeed67c30924879
Juan Miguel Choy <juanchoy30@gmail.com>
2020-04-14 19:29:53 -0300

    Project finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+    //carousel
     $("#mycarousel").carousel( { interval: 2000 } );
+
+    //popover
+    $('[data-toggle="popover"]').popover()
+    $('.popover-dismiss').popover({
+        trigger: 'focus'
+    })
+
+    //Tooltip
+    $('[data-toggle="tooltip"]').tooltip()
 });
